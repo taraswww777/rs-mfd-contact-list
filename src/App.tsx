5a1effae4aab343a99231b9e0b269a911cd81594
taraswww777 <taraswww777@mail.ru>
2023-04-30 17:37:45 +0300

    include bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import {Col, Container, Row, ThemeProvider} from 'react-bootstrap';
 
 export const App = () => {
   return (
-    <div className="App">
-      <h1>rs-mfd-contact-list</h1>
-    </div>
+    <ThemeProvider
+      breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+      minBreakpoint="xxs"
+    >
+      <Container>
+        <Row>
+          <Col
+            xs={2}
+            sm={2}
+            md={2}
+            lg={2}
+            xl={2}
+            xxl={2}
+          >rs-mfd-contact-list</Col>
+        </Row>
+      </Container>
+    </ThemeProvider>
   );
 }
