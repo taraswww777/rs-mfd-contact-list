40e81c3cd02eae52066aea86755f5639cc58da7a
taraswww777 <taraswww777@mail.ru>
2023-05-01 21:59:23 +0300

    change way getting images

--- scripts/src/generators/genPhotoImage.ts
+++ scripts/src/generators/genPhotoImage.ts
@@ -1,24 +1,28 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
+import faker from 'faker'
+import fs from 'fs';
 import path from 'path';
 
-const DOT_ENV = dotenv.config({
-  path: path.join(__dirname, `../../../.env`)
-}).parsed;
+const API_IMAGES_BASE = 'https://picsum.photos';
+const getImgUrlById = (imgId: number, w = 200, h = 300): string => `${API_IMAGES_BASE}/id/${imgId}/${w}/${h}.jpg`;
+const getImgNameById = (imgId: number, w = 200, h = 300): string => `id-${imgId}-${w}-${h}.jpg`;
 
-const REACT_APP_API_NINJAS_TOKEN = DOT_ENV?.REACT_APP_API_NINJAS_TOKEN || ''
-
-
-const API_IMAGES_BASE = 'https://api.api-ninjas.com/v1';
-const API_IMAGES = '/randomimage';
-
-const axiosInstance = axios.create({
-  baseURL: API_IMAGES_BASE,
-  headers: {'X-Api-Key': REACT_APP_API_NINJAS_TOKEN},
-  timeout: 10000,
-});
+const uriImg = '/images';
+const pathToData: string = `../../../public${uriImg}`;
 
 export const genPhotoImage = async (): Promise<string> => {
-  const {data: image} = await axiosInstance.get<string>(API_IMAGES);
-  return `data:image/jpeg;base64,${image}`;
+  const imgId = faker.random.number({min: 1, max: 1000})
+  const imgName = getImgNameById(imgId);
+  const imgUrl = getImgUrlById(imgId);
+
+  console.log('imgUrl:', imgUrl)
+  const response = await axios({
+    url: imgUrl,
+    method: 'GET',
+    responseType: 'stream'
+  });
+
+  response.data.pipe(fs.createWriteStream(path.join(__dirname, pathToData, imgName)))
+
+  return `${uriImg}/${imgName}`;
 }
